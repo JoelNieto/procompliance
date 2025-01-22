af6fe1ec2bf90e68811acf76c9d6878390a02b27
Joel Nieto <joelnieto1215@gmail.com>
2025-01-22 06:16:28 -0500

    proxy

--- apps/web-app/src/app/stores/entity.feature.ts
+++ apps/web-app/src/app/stores/entity.feature.ts
@@ -38,20 +38,22 @@ export function withCustomEntity<T extends { id: EntityId }>(name: string) {
         pipe(
           tap(() => patchState(state, { isLoading: true, error: null })),
           switchMap(() =>
-            state._http.get<T[]>(`/api/${name}`).pipe(
-              tapResponse({
-                next: (items) => patchState(state, setAllEntities(items)),
-                error: (error) => {
-                  state._messageService.add({
-                    severity: 'danger',
-                    detail: 'Algo salio mal, intente de nuevo',
-                    summary: 'Error',
-                  });
-                  console.error(error);
-                },
-                finalize: () => patchState(state, { isLoading: false }),
-              })
-            )
+            state._http
+              .get<T[]>(`https://procompliance.onrender.com/api/${name}`)
+              .pipe(
+                tapResponse({
+                  next: (items) => patchState(state, setAllEntities(items)),
+                  error: (error) => {
+                    state._messageService.add({
+                      severity: 'danger',
+                      detail: 'Algo salio mal, intente de nuevo',
+                      summary: 'Error',
+                    });
+                    console.error(error);
+                  },
+                  finalize: () => patchState(state, { isLoading: false }),
+                })
+              )
           )
         )
       ),
@@ -59,26 +61,28 @@ export function withCustomEntity<T extends { id: EntityId }>(name: string) {
         pipe(
           tap(() => patchState(state, { isLoading: true })),
           switchMap((item) =>
-            state._http.post<T>(`/api/${name}`, item).pipe(
-              tapResponse({
-                next: (item) => {
-                  patchState(state, addEntity(item));
-                  state._messageService.add({
-                    severity: 'success',
-                    detail: 'Elemento creado con exito',
-                    summary: 'Exito',
-                  });
-                },
-                error: (error) => {
-                  state._messageService.add({
-                    severity: 'danger',
-                    detail: 'Algo salio mal, intente de nuevo',
-                    summary: 'Error',
-                  });
-                  console.error(error);
-                },
-              })
-            )
+            state._http
+              .post<T>(`https://procompliance.onrender.com/api/${name}`, item)
+              .pipe(
+                tapResponse({
+                  next: (item) => {
+                    patchState(state, addEntity(item));
+                    state._messageService.add({
+                      severity: 'success',
+                      detail: 'Elemento creado con exito',
+                      summary: 'Exito',
+                    });
+                  },
+                  error: (error) => {
+                    state._messageService.add({
+                      severity: 'danger',
+                      detail: 'Algo salio mal, intente de nuevo',
+                      summary: 'Error',
+                    });
+                    console.error(error);
+                  },
+                })
+              )
           )
         )
       ),
@@ -86,27 +90,35 @@ export function withCustomEntity<T extends { id: EntityId }>(name: string) {
         pipe(
           tap(() => patchState(state, { isLoading: true })),
           switchMap((item) =>
-            state._http.patch<T>(`/api/${name}/${item.id}`, item).pipe(
-              tapResponse({
-                next: (changes) => {
-                  patchState(state, updateEntity({ id: changes.id, changes }));
-                  state._messageService.add({
-                    severity: 'success',
-                    detail: 'Elemento actualizado con exito',
-                    summary: 'Exito',
-                  });
-                },
-                error: (error) => {
-                  state._messageService.add({
-                    severity: 'danger',
-                    detail: 'Algo salio mal, intente de nuevo',
-                    summary: 'Error',
-                  });
-                  console.error(error);
-                },
-                finalize: () => patchState(state, { isLoading: false }),
-              })
-            )
+            state._http
+              .patch<T>(
+                `https://procompliance.onrender.com/api/${name}/${item.id}`,
+                item
+              )
+              .pipe(
+                tapResponse({
+                  next: (changes) => {
+                    patchState(
+                      state,
+                      updateEntity({ id: changes.id, changes })
+                    );
+                    state._messageService.add({
+                      severity: 'success',
+                      detail: 'Elemento actualizado con exito',
+                      summary: 'Exito',
+                    });
+                  },
+                  error: (error) => {
+                    state._messageService.add({
+                      severity: 'danger',
+                      detail: 'Algo salio mal, intente de nuevo',
+                      summary: 'Error',
+                    });
+                    console.error(error);
+                  },
+                  finalize: () => patchState(state, { isLoading: false }),
+                })
+              )
           )
         )
       ),
@@ -114,27 +126,31 @@ export function withCustomEntity<T extends { id: EntityId }>(name: string) {
         pipe(
           tap(() => patchState(state, { isLoading: true })),
           switchMap((id) =>
-            state._http.delete<void>(`/api/${name}/${id}`).pipe(
-              tapResponse({
-                next: () => {
-                  patchState(state, removeEntity(id));
-                  state._messageService.add({
-                    severity: 'success',
-                    detail: 'Elemento eliminado con exito',
-                    summary: 'Exito',
-                  });
-                },
-                error: (error) => {
-                  state._messageService.add({
-                    severity: 'danger',
-                    detail: 'Algo salio mal, intente de nuevo',
-                    summary: 'Error',
-                  });
-                  console.error(error);
-                },
-                finalize: () => patchState(state, { isLoading: false }),
-              })
-            )
+            state._http
+              .delete<void>(
+                `https://procompliance.onrender.com/api/${name}/${id}`
+              )
+              .pipe(
+                tapResponse({
+                  next: () => {
+                    patchState(state, removeEntity(id));
+                    state._messageService.add({
+                      severity: 'success',
+                      detail: 'Elemento eliminado con exito',
+                      summary: 'Exito',
+                    });
+                  },
+                  error: (error) => {
+                    state._messageService.add({
+                      severity: 'danger',
+                      detail: 'Algo salio mal, intente de nuevo',
+                      summary: 'Error',
+                    });
+                    console.error(error);
+                  },
+                  finalize: () => patchState(state, { isLoading: false }),
+                })
+              )
           )
         )
       ),
